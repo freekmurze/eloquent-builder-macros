66d6d00e36d06bd53fcfe89b4bed96bd0880a94c
Kostiantyn Bozhko <bks@exyplis.com>
2018-02-08 11:57:24 +0200

    Add `addSubSelect`, `orderBySub`, `orderBySubDesc` macros from @reinink Laracon 2018 talk

---
+++ src/Macros/orderBySub.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Exyplis\EloquentBuilderMacros\Macros;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Orders sub-query results.
+ *
+ * @author @reinink
+ *
+ * @param Builder $query
+ * @param        $direction
+ *
+ * @return Builder
+ */
+Builder::macro('orderBySub', function ($query, $direction = 'asc') {
+    return $this->orderByRaw("({$query->limit(1)->toSql()}) {$direction}");
+});
---
+++ src/Macros/orderBySubDesc.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Exyplis\EloquentBuilderMacros\Macros;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Orders sub-query results in descending order.
+ *
+ * Originally posted on https://themsaid.com/laravel-query-conditions-20160425
+ * @author @themsaid
+ *
+ * @param Builder $column
+ *
+ * @return Builder
+ */
+Builder::macro('orderBySubDesc', function ($query) {
+    return $this->orderBySub($query, 'desc');
+});
