9c21c4ddf6deaa3e06b80b8b0d20bd44667d1103
Kostyantyn Bozhko <bks@exyplis.com>
2018-02-08 12:02:59 +0200

    Apply fixes from StyleCI (#3)

--- src/Macros/addSubSelect.php
+++ src/Macros/addSubSelect.php
@@ -4,7 +4,7 @@ namespace Exyplis\EloquentBuilderMacros\Macros;
 
 use Illuminate\Database\Eloquent\Builder;
 
-/**
+/*
  * Adds sub query to exiting one.
  *
  * @author @reinink
@@ -18,5 +18,6 @@ Builder::macro('addSubSelect', function ($column, $query) {
     if (is_null($this->getQuery()->columns)) {
         $this->select($this->getQuery()->from.'.*');
     }
+
     return $this->selectSub($query->limit(1)->getQuery(), $column);
 });
